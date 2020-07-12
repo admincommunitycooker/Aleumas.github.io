7be4d6c31899e2b57be1b7a2e4e4921174781e84
sallysfurniture <68182736+sallysfurniture@users.noreply.github.com>
2020-07-12 14:24:15 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,20 +11,14 @@
 		$sidebar = $('#sidebar');
 
 	// Breakpoints.
-		//breakpoints({
-		//	xlarge:   [ '1281px',  '1680px' ],
-		//	large:    [ '981px',   '1280px' ],
-		//	medium:   [ '737px',   '980px'  ],
-		//	small:    [ '481px',   '736px'  ],
-		//	xsmall:   [ null,      '480px'  ]
-//		});
-			breakpoints({
-			xlarge:   [ '981px',  '1280px' ],
-			large:    [ '737px',   '980px' ],
-			medium:   [ '481px',   '736px'  ],
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
 			xsmall:   [ null,      '480px'  ]
 		});
+
 
 
 	// Hack: Enable IE flexbox workarounds.
